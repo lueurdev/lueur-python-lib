499592f9a93cfd55b40c3df0fddcd05c1a0a93aa
Sylvain Hellegouarch <sh@defuze.org>
2024-09-06 23:15:55 +0200

    add expand_links for kubernetes
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,15 @@
 
 ## [Unreleased][]
 
-[Unreleased]: https://github.com/lueurdev/lueur/compare/0.5.1..HEAD
+[Unreleased]: https://github.com/lueurdev/lueur/compare/0.6.0..HEAD
+
+## [0.6.0][] - 2024-09-06
+
+[0.6.0]: https://github.com/lueurdev/lueur/compare/0.5.1..0.6.0
+
+### Added
+
+-   Kubernetes expand_links
 
 ## [0.5.1][] - 2024-09-06
 
--- lueur/platform/k8s/__init__.py
+++ lueur/platform/k8s/__init__.py
@@ -1,18 +1,27 @@
 import asyncio
 import secrets
+from typing import Any
 
 from lueur.make_id import make_id
 from lueur.models import Discovery, Meta
+from lueur.platform.k8s.deployment import (
+    expand_links as deployment_expand_links,
+)
 from lueur.platform.k8s.deployment import explore_deployment
+from lueur.platform.k8s.gateway import expand_links as gateway_expand_links
 from lueur.platform.k8s.gateway import explore_gateway
 from lueur.platform.k8s.ingress import explore_ingress
 from lueur.platform.k8s.network_policy import explore_network_policy
+from lueur.platform.k8s.node import expand_links as node_expand_links
 from lueur.platform.k8s.node import explore_node
 from lueur.platform.k8s.pod import explore_pod
+from lueur.platform.k8s.replicaset import (
+    expand_links as replicaset_expand_links,
+)
 from lueur.platform.k8s.replicaset import explore_replicaset
 from lueur.platform.k8s.service import explore_service
 
-__all__ = ["explore"]
+__all__ = ["explore", "expand_links"]
 
 
 async def explore() -> Discovery:
@@ -42,3 +51,10 @@ async def explore() -> Discovery:
         resources=resources,
         meta=Meta(name=name, display="Kubernetes", kind="k8s"),
     )
+
+
+def expand_links(d: Discovery, serialized: dict[str, Any]) -> None:
+    deployment_expand_links(d, serialized)
+    gateway_expand_links(d, serialized)
+    node_expand_links(d, serialized)
+    replicaset_expand_links(d, serialized)
